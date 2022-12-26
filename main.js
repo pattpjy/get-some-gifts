fdef596b8500ae44412507f87f02df40bdc10dbe
Patt Sookmark <pijiya@sookmark.com>
2022-12-25 21:29:37 -0700

    Add fetch, checkbox and recalc total

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 //Global Variables
 let giftList 
 let totalAmount
-
-const recName = document.querySelector('#recipient')
-const itemName = document.querySelector('#item')
-const priceItem = document.querySelector('#price')
+let inputCheck 
+// const recName = document.querySelector('#recipient')
+// const itemName = document.querySelector('#item')
+// const priceItem = document.querySelector('#price')
 const bodyTable = document.querySelector('#tableBody')
 const dollarAmt = document.querySelector('#totalDollar')
+
 
 //EventListener
 window.addEventListener('load',function(){
     getGiftdata()
-}
-)
+})
+//add eventListerner to quesrySeletorAll
+
+
 
 //Functions
 function getGiftdata() {
@@ -21,6 +24,8 @@ function getGiftdata() {
       giftList = data
       sumPrice()
       addData()
+      addCheck()
+
       return giftList
     })
   }
@@ -44,17 +49,20 @@ function addData(){
     trTag.appendChild(tdInput)
     tdInput.appendChild(inputTag)
 
-    tdName.innerHTML = giftList[i].recipient
-    tdItem.innerHTML = giftList[i].name
-    tdPrice.innerHTML = giftList[i].priceInDollars
+    tdName.innerText = giftList[i].recipient
+    tdItem.innerText = giftList[i].name
+    tdPrice.innerText = giftList[i].priceInDollars
+    inputTag.id = giftList[i].id
     
-    dollarAmt.innerHTML = totalAmount
-    // tdName.innerHTML = giftList[i].recipient
-    // tdItem.innerHTML = giftList[i].name
-    // tdPrice.innerHTML = giftList[i].priceInDollars
-};
-   
+    dollarAmt.innerText = totalAmount
+  
+  };
 }
+function addCheck() {
+  inputCheck = document.querySelectorAll('INPUT')
+  inputCheck.forEach((el)=> el.addEventListener('click', deductPrice))
+}
+   
 function sumPrice(){
     totalAmount = giftList.reduce((acc,curr)=>{
     acc+= curr.priceInDollars 
@@ -62,3 +70,20 @@ function sumPrice(){
     } ,0)
     return totalAmount
 }
+
+function deductPrice(){
+  // checkbox is checked === true
+  if (this.checked) {
+    console.log("Checkbox is checked..");
+    totalAmount = totalAmount -  giftList.find((key)=>key.id.toString()===this.id.toString()).priceInDollars
+    dollarAmt.innerText = totalAmount
+  } else {
+    totalAmount = totalAmount + giftList.find((key)=>key.id.toString()===this.id.toString()).priceInDollars
+    dollarAmt.innerText = totalAmount
+  }
+    // return checkbox id 
+    // giftList with the same id, return gift price
+    // deduct gift rpice from totalAMount  
+}
+//Decrease the price when check the box
+// use quesryselectAll and eventListern checked === true, then match id or something to refence which items and deduce from total amount (Puff you need to add id to the newly create checkBox)
